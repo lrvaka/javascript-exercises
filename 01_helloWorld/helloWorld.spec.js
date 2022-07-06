635670f47aa314e5f7182de75455778ba3dd907e
lrvaka <lukevakasisikakala@gmail.com>
2022-07-06 04:36:15 -0400

    complete exercise 1

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -1,7 +1,12 @@
-const helloWorld = require('./helloWorld');
+const helloWorld = require("./helloWorld")
 
-describe('Hello World', function() {
-  test('says "Hello, World!"', function() {
-    expect(helloWorld()).toEqual('Hello, World!');
-  });
-});
+//describe method groups together related tests
+describe("Hello World", () => {
+  //test method used to for testing
+  //The first argument is the test name; the second argument is a function that 
+  //contains the expectations to test. The third argument (optional) is timeout (in milliseconds) 
+  //for specifying how long to wait before aborting. Note: The default timeout is 5 seconds.
+  test('says "Hello, World!"', () => {
+    expect(helloWorld()).toEqual("Hello, World!")
+  })
+})
