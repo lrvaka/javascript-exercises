90c99681753563041a82b189db20d0719ebcb321
lrvaka <lukevakasisikakala@gmail.com>
2022-07-06 05:16:58 -0400

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = (arr, ...elements) => {
+  elements.forEach((e) => {
+    if (arr.indexOf(e) === -1) return
+    arr.splice(arr.indexOf(e), 1)
+  })
 
-};
+  return arr
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
