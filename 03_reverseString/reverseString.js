643b2dcd0c6688ce58d30e875e7c02a6f3932c5d
lrvaka <lukevakasisikakala@gmail.com>
2022-07-06 04:56:45 -0400

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = (string) => {
+  //inputs - string
+  //outputs - reversal of string
+  const arrayOfString = string.split("")
+  let reversedArrayOfString = []
+  arrayOfString.forEach((e) => reversedArrayOfString.unshift(e))
+  return reversedArrayOfString.join("")
+}
 
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString
