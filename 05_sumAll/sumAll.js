6dd5a7500c9490f742cbb5786ede3c1892c640fa
lrvaka <lukevakasisikakala@gmail.com>
2022-07-06 05:37:00 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = (a, b) => {
+  if (a < 0 || b < 0) return "ERROR"
+  if (a < 0 || b < 0) return "ERROR"
+  if (typeof a !== "number" || typeof b !== "number") return "ERROR"
 
-};
+  let range = []
+
+  if (a > b) {
+    for (let i = b; i <= a; i++) {
+      range.push(i)
+    }
+  } else {
+    for (let i = a; i <= b; i++) {
+      range.push(i)
+    }
+  }
+
+  const sum = range.reduce((p, c) => p + c)
+  return sum
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
