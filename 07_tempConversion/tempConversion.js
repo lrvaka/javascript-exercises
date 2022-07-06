67bce5ff2ecc431e38b0bda463418b662ce618b4
lrvaka <lukevakasisikakala@gmail.com>
2022-07-06 06:04:30 -0400

    complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
+const ftoc = (fahrenheit) => {
+  if ((fahrenheit - 32) * (5 / 9) === 0) return 0
+  return +((fahrenheit - 32) * (5 / 9)).toFixed(1)
+}
 
-};
-
-const ctof = function() {
-
-};
+const ctof = (celsius) => {
+  if (celsius * (9 / 5) + 32 === 0) return 0
+  return +(celsius * (9 / 5) + 32).toFixed(1)
+}
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
-};
+  ctof,
+}
