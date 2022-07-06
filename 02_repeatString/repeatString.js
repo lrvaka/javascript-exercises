61267a6b85080d8638a71caafec03d9e556c263f
lrvaka <lukevakasisikakala@gmail.com>
2022-07-06 04:44:09 -0400

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = (string, num) => {
+  if (num < 0) return "ERROR"
 
-};
+  let endResult = ""
+  for (let i = 0; i < num; i++) {
+    endResult += string
+  }
+  return endResult
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
